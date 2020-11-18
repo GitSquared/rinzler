5de6a19d3663ba91ba05064fda50d7d968000aca
Gabriel Saillard <gabriel@saillard.dev>
2020-11-19 00:26:51 +0100

    feat: job scheduler, managing load distribution across a pool of workers

---
+++ src/scheduler.ts
@@ -0,0 +1,57 @@
+import type WebWorker from './worker-wrapper'
+import type { JobCall } from './worker-wrapper'
+import { nanoid } from 'nanoid'
+
+export default class Scheduler {
+	/* Public props */
+
+	workerPool: Map<string, WebWorker> = new Map()
+
+	/* Private props */
+
+	/* Public methods */
+
+	async extendPool(worker: WebWorker): Promise<string> {
+		const id = nanoid()
+		await worker.start()
+		this.workerPool.set(id, worker)
+		return id
+	}
+
+	async reducePool(): Promise<void> {
+		const [id, worker] = this._getLeastBusyWorker()
+		if (!worker) return
+		await worker.shutdown()
+		this.workerPool.delete(id)
+	}
+
+	async submitJob(message: unknown, transfer?: Transferable[]): Promise<[string, string]> {
+		const id = nanoid()
+		const job: JobCall = {
+			type: 'job',
+			id,
+			message,
+			transfer
+		}
+
+		const [wid, worker] = this._getLeastBusyWorker()
+		await worker.submitJob(job)
+		return [id, wid]
+	}
+
+	/* Private methods */
+
+	private _getLeastBusyWorker(): [string, WebWorker] {
+		let lowest: [number, string] = [-1, '']
+		// O(n) complexity except when a worker has 0 current jobs,
+		// but we're unlikely to ever be looping >20 entries, so...
+		for (const entry of this.workerPool) {
+			if (lowest[0] === -1 || entry[1].jobs.length < lowest[0]) {
+				if (!entry[1].active) continue
+				lowest = [entry[1].jobs.length, entry[0]]
+				if (lowest[0] === 0) break
+			}
+		}
+		return [lowest[1], this.workerPool.get(lowest[1]) as WebWorker]
+	}
+}
