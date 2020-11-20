5e1b3d2d341c3596b8fd979eaa0c3152501c0a7d
Gabriel Saillard <gabriel@saillard.dev>
2020-11-20 17:05:30 +0100

    feat: engine: automatic cooldown

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,14 @@ export default class RinzlerEngine {
 
 	#maxTemp: number
 	#targetTemp = -1
+	#minTemp = 1
 	#workerArgs: Parameters<RinzlerEngine['startup']> | undefined
 	#scheduler: Scheduler = new Scheduler()
+	#coolingTimer: [number, string] | null = null
+	#extendPoolTimes: number[] = []
+
+	readonly #coolingDelay = 4000
+	readonly #extendPoolTimesBacklogSize = 20
 
 	/* Public methods */
 
@@ -27,10 +33,9 @@ export default class RinzlerEngine {
 		await this._coolDown(this.#scheduler.workerPool.size)
 	}
 
-	//TODO: automatic horizontal scaling, job error handling
-	async runJob(message: unknown, transfer?: Transferable[]): Promise<[message: unknown, transfer?: Transferable[]]> {
-		const jobResults = await this.#scheduler.submitJob(message, transfer)
-		return [jobResults.message, jobResults.transfer]
+	//TODO: job error handling
+	runJob(message: unknown, transfer?: Transferable[]): Promise<[message: unknown, transfer?: Transferable[]]> {
+		return this.#scheduler.submitJob(message, transfer)
 	}
 
 	async preHeat(): Promise<RinzlerEngine> {
@@ -38,12 +43,35 @@ export default class RinzlerEngine {
 		return this
 	}
 
+	//TODO: afterburner?
+
 	/* Internal methods */
-	// ESnext private class methods are not yet handled correctly in TypeScript, so we use soft-private for now...
+	// ES6 private class methods are not yet handled correctly in TypeScript, so we use soft-private for now...
 	// Tracked here: https://github.com/microsoft/TypeScript/issues/37677
 
 	private _measureTemp(): number {
 		return this.#scheduler.workerPool.size
+	}
+
+	private _idleWorkerListener(wid: string): void {
+		if (this.#coolingTimer) return
+		const worker = this.#scheduler.workerPool.get(wid)
+
+		this.#coolingTimer = [window.setTimeout(() => {
+			this.#scheduler.reducePool(wid)
+		}, this.#coolingDelay), wid]
+
+		worker?.once('jobok', () => {
+			if (this.#coolingTimer && this.#coolingTimer[1] === wid) {
+				window.clearTimeout(this.#coolingTimer[0])
+				const { wid, jobCount } = this.#scheduler.getLeastBusyWorker()
+				if (jobCount === 0) {
+					this._idleWorkerListener(wid)
+				} else {
+					this.#coolingTimer = null
+				}
+			}
+		})
 	}
 
 	private async _heatUp(threadCount = 1): Promise<void> {
@@ -54,12 +82,23 @@ export default class RinzlerEngine {
 
 		this.#targetTemp = (this._measureTemp() + threadCount) || threadCount
 		for (let i = 0; i < threadCount; i++) {
-			await this.#scheduler.extendPool(new WebWorker(...this.#workerArgs))
+			const perfMarkA = performance.now()
+
+			const worker = new WebWorker(...this.#workerArgs)
+			const wid = await this.#scheduler.extendPool(worker)
+			worker.on('idle', () => {
+				this._idleWorkerListener(wid)
+			})
+
+			const perfMarkB = performance.now()
+			this.#extendPoolTimes.push(perfMarkB - perfMarkA)
+			this.#extendPoolTimes.splice(this.#extendPoolTimesBacklogSize, this.#extendPoolTimes.length - this.#extendPoolTimesBacklogSize)
 		}
 	}
 
 	private async _coolDown(threadCount = 1): Promise<void> {
 		// reduce number of active workers
+		if ((this.#targetTemp - threadCount) < this.#minTemp) throw new Error('Rinzler (internal): attempting to underheat')
 		if ((this.#targetTemp - threadCount) < this._measureTemp()) throw new Error('Rinzler (internal): engine cannot run in subzero temperatures')
 
 		this.#targetTemp = this._measureTemp() - threadCount
