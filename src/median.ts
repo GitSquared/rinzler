21c96436f2ff91a9e825974fd1c6448d0e41bc3a
Gabriel Saillard <gabriel@saillard.dev>
2022-01-03 12:06:22 +0100

    fix: wrong median calculation on odd-length arrays

--- src/median.ts
+++ src/median.ts
@@ -1,6 +1,6 @@
 export default function calculateMedian(arr: number[]): number {
 	if (!arr.length) return 0
-	const mid = Math.floor(arr.length /2)
+	const mid = Math.ceil(arr.length /2)
 	const nums = [...arr].sort((a, b) => a - b)
 	return (arr.length % 2 !== 0) ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2
 }
